a7b2f2c55e5f4fd92193cb0f3192540b71fddf6a
Adem Gaygusuz <adem@ardweb.co.uk>
2016-10-26 22:59:13 +0100

    add features-service.appyharry to allowed hosts

--- service/settings.py
+++ service/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'features-service.appyharry.com'
+]
 
 
 # Application definition
