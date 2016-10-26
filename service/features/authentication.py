980a3d359274b05bb6d7f0ee73339b76a69144fd
Adem Gaygusuz <adem@ardweb.co.uk>
2016-10-26 22:55:45 +0100

    Revert "Add card exempt authentication"
    This reverts commit 47823bf82414774b72b082327719726faf5082f9.

--- service/features/authentication.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
-class JSONWebTokenCsrfExemptAuthentication(JSONWebTokenAuthentication):
-
-    def enforce_csrf(self, request):
-        return
--- service/settings.py
+++ service/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'features.authentication.JSONWebTokenCsrfExemptAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
