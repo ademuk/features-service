39bd2b9fe04ebe1e272448443efcc6ed335ff8a9
Adem Gaygusuz <adem@ardweb.co.uk>
2016-10-26 22:14:19 +0100

    fix auth

--- service/settings.py
+++ service/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'features.authentication.JSONWebTokenCsrfExemptAuthentication',
+        'service.features.authentication.JSONWebTokenCsrfExemptAuthentication',
     ),
 }
 
