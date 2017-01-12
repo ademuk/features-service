291bc461f7f849b9635fa8d67d17f970cfb6b44a
Adem Gaygusuz <adem@ardweb.co.uk>
2017-01-12 02:00:48 +0000

    Revert "Cat key to logs"
    This reverts commit 6d42cd70c2d45e6d89c673423c3864fc45e88c35.

--- service/features/importer.py
+++ service/features/importer.py
@@ -43,7 +43,7 @@ class GitFeatureImporter:
             os.chmod(key_path, 0o600)
 
             env = os.environ.copy()
-            env["GIT_SSH_COMMAND"] = "cat %s;ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i %s" % (key_path, key_path)
+            env["GIT_SSH_COMMAND"] = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i %s" % key_path
             cmd = ["git", "clone", self.project.repo_url, repo_path]
 
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
@@ -51,7 +51,7 @@ class GitFeatureImporter:
             stdout, stderr = p.communicate()
 
             print(env["GIT_SSH_COMMAND"])
-
+            
             if stdout:
                 print(stdout)
             if stderr:
